b8052aaa2b8474a1bbb946657b33a3d69020c739
bkwan-wcm <bkwan@med.cornell.edu>
2026-01-21 07:01:02 -0500

    Update streamlit_app.py
    adding subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,8 @@ if ingredients_list:   #actually means if ingredients_list is not null#
         #+= means add this to what is already in the variable #
         #Ingredients_list and Ingredient_string are different varibles, dont mix them up #
         ingredients_string+=fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_dt=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
                        
 
