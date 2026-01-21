583c6deb9ab039f1c3713511d905f379c4bdc768
bkwan-wcm <bkwan@med.cornell.edu>
2026-01-21 06:44:03 -0500

    Update streamlit_app.py
    correcting the container syntex

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,9 @@ if ingredients_list:   #actually means if ingredients_list is not null#
 #New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json(),use_container_width=true)
+#st.text(smoothiefroot_response.json())
+sf_dt=st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
+
 
 
         
